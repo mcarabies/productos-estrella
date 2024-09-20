85f4d3f6ecbe7f03177a2aa7e3c6b109eac9258e
mcarabies <marcelojcarabies@gmail.com>
2024-09-20 10:13:28 -0300

    model images y display de images en en admin finalizado

--- core/admin.py
+++ core/admin.py
@@ -1,37 +1,65 @@
 from django.contrib import admin
-from .models import Category, Tag, Product, About, Linksocial
+from .models import Category, Tag, Product, About, Linksocial, Productimages
 
 # Register your models here.
 # CATEGORIAS
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
     list_display = ('name', 'active')
-    
-admin.site.register(Category, CategoryAdmin)
 
 # ETIQUETAS
+@admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
     list_display = ('name', 'active')
-admin.site.register(Tag, TagAdmin)
 
 # PRODUCTOS
+class ImagesInstanceInLine(admin.TabularInline):
+    model = Productimages
+    
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (
+            'Contenido', {
+                'fields': ('title',('excerpt','content'), ('created', 'updated'),),
+            }
+        ),
+        (
+            'Amazon',{
+                'fields': (('url_amazon','url_afiliado_amazon'),)
+            }    
+        ),
+       
+    )
     readonly_fields = ('created', 'updated')
     list_display = ('published', 'title', 'category', 'created' )
     ordering = ('published','-created', 'title')
     search_fields = ('title',)
-admin.site.register(Product, ProductAdmin)
+    inlines = [ImagesInstanceInLine]
+    #@admin.display(description ='url')
+    #def images(self):
+     #   images_list = []
+      #  images_list = Productimages.objects.filter(Product)
+       # print(images_list)
+        #return images_list
 
 # ACERCA DE NOSOTROS
+@admin.register(About)
 class AboutAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
     list_display = ('description',)
-admin.site.register(About, AboutAdmin)
 
 # REDES SOCIALES
+@admin.register(Linksocial)
 class LinksocialAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
     list_display = ('name', 'url')
     search_fields = ('title',)
-admin.site.register(Linksocial, LinksocialAdmin)
+
+# IMAGENES DE PRODUCTOS
+@admin.register(Productimages)
+class ProductimagesAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+    list_display = ('product', 'url')
